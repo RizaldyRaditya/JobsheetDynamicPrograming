5d1ea28d8af5bea1a8e471fadadab5a7a4178440
Rudi Erwanto <Rudi Erwanto@DESKTOP-86HM3LO>
2018-09-12 12:42:49 +0700

    Initial Commit

---
+++ src/Latihan/HitungFibonacci.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Rudi Erwanto
+ */
+public class HitungFibonacci {
+    public static void main(String[] args) {
+        String identitas = "Rizaldy Raditya Erwanto / X RPL 5 / 36";
+        
+       tampilJudul(identitas);
+    }
+    private static void tampilJudul(String Identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, .... dst.\n");
+    }
+}
