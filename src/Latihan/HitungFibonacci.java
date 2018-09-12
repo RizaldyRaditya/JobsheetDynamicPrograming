648fee843a61707c983d54e485daf171831098c7
Rudi Erwanto <Rudi Erwanto@DESKTOP-86HM3LO>
2018-09-12 12:47:21 +0700

    Tambah Input

--- src/Latihan/HitungFibonacci.java
+++ src/Latihan/HitungFibonacci.java
@@ -5,20 +5,32 @@
  */
 package Latihan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rudi Erwanto
  */
 public class HitungFibonacci {
-    public static void main(String[] args) {
-        String identitas = "Rizaldy Raditya Erwanto / X RPL 5 / 36";
-        
-       tampilJudul(identitas);
-    }
     private static void tampilJudul(String Identitas) {
-        System.out.println("Identitas : "+ identitas);
+        System.out.println("Identitas : "+Identitas);
         
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, .... dst.\n");
     }
+     public static void main(String[] args) {
+        String identitas = "Rizaldy Raditya Erwanto / X RPL 5 / 36";
+        
+       tampilJudul(identitas);
+       int n = tampilInput();
+    }
+     private static int tampilInput() {
+         Scanner input = new Scanner(System.in);
+      
+         System.out.println("Bilangan k-: ");
+         int n = input.nextInt();
+         
+         return n;
+     }
+     
 }
